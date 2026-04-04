bcf0829d26f3af84a599eaf62fd7c3add78f0176
Ariel Venur <141952214+DEVenur@users.noreply.github.com>
2026-04-04 18:13:40 -0300

    Add GoogleDriveAudioTrack class for audio handling

---
+++ source-managers/src/main/java/com/dunctebot/sourcemanagers/googledrive/GoogleDriveAudioTrack.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 Duncan "duncte123" Sterken
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.dunctebot.sourcemanagers.googledrive;
+
+import com.dunctebot.sourcemanagers.AbstractDuncteBotHttpSource;
+import com.dunctebot.sourcemanagers.Mp3Track;
+import com.sedmelluq.discord.lavaplayer.container.mp3.Mp3AudioTrack;
+import com.sedmelluq.discord.lavaplayer.container.mpeg.MpegAudioTrack;
+import com.sedmelluq.discord.lavaplayer.container.ogg.OggAudioTrack;
+import com.sedmelluq.discord.lavaplayer.container.wav.WavAudioTrack;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.tools.io.SeekableInputStream;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import com.sedmelluq.discord.lavaplayer.track.InternalAudioTrack;
+
+public class GoogleDriveAudioTrack extends Mp3Track {
+
+    public GoogleDriveAudioTrack(AudioTrackInfo trackInfo, AbstractDuncteBotHttpSource manager) {
+        super(trackInfo, manager);
+    }
+
+    @Override
+    protected InternalAudioTrack createAudioTrack(AudioTrackInfo trackInfo, SeekableInputStream stream) {
+        final String mimeType = this.trackInfo.artworkUrl; // we reuse artworkUrl to carry mimeType
+
+        if (mimeType == null) {
+            return new Mp3AudioTrack(trackInfo, stream);
+        }
+
+        switch (mimeType) {
+            case "audio/mpeg":
+            case "audio/mp3":
+                return new Mp3AudioTrack(trackInfo, stream);
+            case "audio/mp4":
+            case "audio/x-m4a":
+                return new MpegAudioTrack(trackInfo, stream);
+            case "audio/ogg":
+                return new OggAudioTrack(trackInfo, stream);
+            case "audio/wav":
+            case "audio/x-wav":
+                return new WavAudioTrack(trackInfo, stream);
+            default:
+                throw new FriendlyException(
+                        "Unsupported audio format: " + mimeType,
+                        FriendlyException.Severity.COMMON,
+                        null
+                );
+        }
+    }
+
+    @Override
+    public String getPlaybackUrl() {
+        return GoogleDriveAudioSourceManager.getDownloadUrl(this.trackInfo.identifier);
+    }
+
+    @Override
+    public GoogleDriveAudioSourceManager getSourceManager() {
+        return (GoogleDriveAudioSourceManager) super.getSourceManager();
+    }
+}
